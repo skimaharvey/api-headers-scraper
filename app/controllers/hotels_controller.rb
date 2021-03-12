e25897928a2f79cc6e3154906ea3773a8b10db9c
ski <ski@skis-MacBook-Pro.local>
2021-03-12 23:25:25 +0100

    test

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,7 +1,7 @@
 class HotelsController < ApplicationController
 
     def create 
-        @hotel = Hotel.create(hotel_name: params["name"], hotel_reservation_code: params[:hotel_reservation_code])
+        @hotel = Hotel.create(hotel_name: params["name"], hotel_reservation_code: params["hotel_reservation_code"])
         render json: {"hotel_id": @hotel}, status: 200
     end
 end
