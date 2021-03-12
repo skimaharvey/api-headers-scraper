9ecc8f3eccf4d48b39d4afc077f9d7141b4fcd14
ski <ski@skis-MacBook-Pro.local>
2021-03-12 15:23:59 +0100

    added prices method to test on prod

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,8 +1,15 @@
 class PricesController < ApplicationController
+  # before_action :create
   def new
   end
 
   def create
+    @price = Price.new(price: params[:price], available: params[:available], n_of_units_available: params[:n_of_units_available])
+    if @price.save 
+      render json: @price
+    else 
+      render json: {error: 'not working'}, status: 400
+    end
   end
 
   def update
@@ -10,12 +17,15 @@ class PricesController < ApplicationController
 
   def destroy
   end
-  def test 
-    @prices = Price.all
-    render :json => {"message": "hello"}
-  end
+
   def index
-    @prices 
-    render render :json => @prices
+    @prices = Price.all
+    render  :json => @prices
   end
+
+  private 
+
+  # def price_params
+  #   params.require(:price).permit(:available, :n_of_units_available)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'prices/new'
-  get 'prices/create'
-  get 'prices/update'
-  get 'prices/destroy'
-  get 'prices/index'
-  get '/prices', to: 'prices#test'
+
+  get '/prices', to: 'prices#index'
+  post '/prices', to: 'prices#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
