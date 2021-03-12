373646d4a194868be23bfa4c4ad09ad5534a83bf
ski <ski@skis-MacBook-Pro.local>
2021-03-12 23:35:39 +0100

    fdff

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,7 +1,11 @@
 class HotelsController < ApplicationController
 
     def create 
-        @hotel = Hotel.create(hotel_name: params["name"], hotel_reservation_code: params["hotel_reservation_code"])
+        begin
+        @hotel = Hotel.create(name: params["name"], hotel_reservation_code: params["hotel_reservation_code"])
         render json: {"hotel_id": @hotel}, status: 200
+        rescue => error
+            render json: {"message", error}, status: 400
+        end
     end
 end
