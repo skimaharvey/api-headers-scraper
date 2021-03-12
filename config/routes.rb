33aaf7cbce80c768e577105fcf24ee161f92ed51
ski <ski@skis-MacBook-Pro.local>
2021-03-12 23:22:55 +0100

    hotel controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/prices', to: 'prices#index'
   post '/prices', to: 'prices#create'
   post '/scrape_reservit', to: 'scraping_sessions#scrape_reservit'
+  post '/new_hotel', to: 'hotels#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
-rm -f /myapp/tmp/pids/server2.pid
+rm -f /myapp/tmp/pids/server2431.pid
 bundle exec rake db:exists || bundle exec rake db:create
 bundle exec rake db:migrate
 # Then exec th container's main process (what's set as CMD in the Dockerfile).
