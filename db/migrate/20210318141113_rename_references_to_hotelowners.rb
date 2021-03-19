7e0de32f0bc4bc234a05bd5ec8d22458b4c875aa
ski <ski@skis-MacBook-Pro.local>
2021-03-19 20:40:24 +0100

    test

--- db/migrate/20210318141113_rename_references_to_hotelowners.rb
+++
@@ -1,8 +0,0 @@
-class RenameReferencesToHotelowners < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :hotel_owners, :user_id_id, :user_id
-    rename_column :hotel_owners, :hotel_id_id, :hotel_id
-    rename_column :hotel_competitors, :user_id_id, :user_id
-    rename_column :hotel_competitors, :hotel_id_id, :hotel_id
-  end
-end
