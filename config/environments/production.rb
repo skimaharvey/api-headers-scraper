c356008197961e0b43f95cd64e1439c695317277
ski <ski@skis-MacBook-Pro.local>
2021-03-23 10:52:58 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_cable.disable_request_forgery_protection = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
