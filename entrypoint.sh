423e9426570ff2ed97e5fa78fbfcd02eecf04c88
ski <ski@skis-MacBook-Pro.local>
2021-03-27 22:15:16 +0100

    trst

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 rm -f /myapp/tmp/pids/server.pid
 rm -f /myapp/tmp/pids/server2431.pid
 bundle exec rake db:exists || bundle exec rake db:create
-# bundle exec rake db:migrate
+bundle exec rake db:migrate
 # Then exec th container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
