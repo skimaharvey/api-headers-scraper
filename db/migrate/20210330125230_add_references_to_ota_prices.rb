25ae6552129ee3b22c624f285e8d4d6967b8eaf9
ski <ski@skis-MacBook-Pro.local>
2021-03-31 00:04:49 +0200

    fixing migration issue

--- db/migrate/20210330125230_add_references_to_ota_prices.rb
+++ db/migrate/20210330125230_add_references_to_ota_prices.rb
@@ -1,6 +1,6 @@
 class AddReferencesToOtaPrices < ActiveRecord::Migration[5.2]
   def change
     add_reference :ota_prices, :hotel, foreign_key: true
-    add_reference :ota_prices, :date_of_price_id, foreign_key: true
+    add_reference :ota_prices, :date_of_price, foreign_key: true
   end
 end
--- db/migrate/20210330205547_rename_dateofpriceid_to_ota_prices.rb
+++ db/migrate/20210330205547_rename_dateofpriceid_to_ota_prices.rb
@@ -1,5 +1,5 @@
 class RenameDateofpriceidToOtaPrices < ActiveRecord::Migration[5.2]
   def change
-    rename_column :ota_prices, :date_of_price_id_id, :date_of_price_id
+    # rename_column :ota_prices, :date_of_price_id_id, :date_of_price_id
   end
 end
