a022d916cc644d9133b5afc28a731464e25ec8cb
ski <ski@skis-MacBook-Pro.local>
2021-04-03 21:34:46 +0200

    trying to make sentry get the application errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,7 @@
 Sentry.init do |config|
     config.dsn = 'https://f3623adfa9a4432495266b42dd141b4d@o563551.ingest.sentry.io/5703636'
     config.breadcrumbs_logger = [:sentry_logger]
+    config.async = lambda do |event, hint|
+        Sentry::SendEventJob.perform_later(event, hint)
+    end
   end
