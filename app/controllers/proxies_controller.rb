0610afb39ae4fd8d957c9c6a4585cab19b1ab214
ski <ski@skis-MacBook-Pro.local>
2021-04-19 12:52:04 +0200

    error with proxy password field

--- app/controllers/proxies_controller.rb
+++ app/controllers/proxies_controller.rb
@@ -1,17 +1,17 @@
 class ProxiesController < ApplicationController
     def create
-        new_proxy = Proxy.create(proxy_body: params['proxy_body'], port: params['port'].to_i, password: params['password'],
+        new_proxy = Proxy.create(proxy_body: params['proxy_body'], port: params['port'].to_i, proxy_pass: params['password'],
         username: params['username'])
-        render json: {"proxy": new_proxy}
+        render json: {"proxy": new_proxy}, status: 200
     end
 
     def index
         all_proxies = Proxy.all
-        render json: {"proxy": all_proxies}
+        render json: {"proxy": all_proxies}, status: 200
     end
 
     def delete 
         Proxy.where(id: params["proxies_ids"]).destroy_all
-        render json: {"message": "Proxies deleted"}
+        render json: {"message": "Proxies deleted"}, status: 200
     end
 end
